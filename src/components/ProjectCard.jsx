d834f2cfff0235bc95e43e6bc88b13d44eb58652
monishkumardhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2024-09-07 15:42:18 -0400

    Completed portfolio updated website header icon

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,9 +7,9 @@ export const ProjectCard = ({ title, description, imgUrl, url }) => {
       <div className="relative z-10 rounded-2xl overflow-hidden border-4 border-gray-800 transition-transform transform hover:scale-105">
         <img src={imgUrl} alt={title} className="w-full h-full object-cover" />
         <div className="absolute inset-0 flex items-center justify-center bg-gradient-to-r from-[#AA367C] via-[#AA367C] to-[#4A2FBD] opacity-0 hover:opacity-80 transition-opacity">
-          <div className="text-center text-white px-4 py-2">
-            <h4 className="text-2xl font-bold mb-2">{title}</h4>
-            <span className="italic">{description}</span>
+          <div className="text-center text-white px-2 sm:px-4 py-2">
+            <h4 className="text-lg sm:text-2xl font-bold mb-1 sm:mb-2">{title}</h4>
+            <span className="text-sm sm:text-base italic">{description}</span>
           </div>
         </div>
       </div>
