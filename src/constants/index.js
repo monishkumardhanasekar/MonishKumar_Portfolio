b925a36fed4660b102791a5341fda0b955cf3b9e
monishkumardhanasekar <145507012+monishkumardhanasekar@users.noreply.github.com>
2025-04-22 21:15:43 -0400

    updated times

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,17 +5,43 @@ export const ABOUT_TEXT = `I’m Monish Kumar Dhanasekar, a software engineer wh
 
 export const EDUCATION_TIMELINE = [
     {
-        date: 'May 2018 - May 2022',
-        title: 'Bachelor of Engineering in Electrical and Electronics',
-        subtitle: 'Anna University, India',
+        "date": "September 2024 - December 2024",
+        "title": "Software Development Intern",
+        "subtitle": "Entertainment Technologists Development Corp., California, USA",
+        "details": [
+            "Engineered AI/ML models using TensorFlow, Keras, Pandas, and OpenCV to optimize media processes including image processing, video rendering, and content analysis",
+            "Increased model efficiency by 15% through fine-tuning algorithms for processing media inputs",
+            "Integrated front-end interfaces with AI/ML models, improving input data workflows and reducing processing time by 20%"
+        ],
+        "iconStyle": { "background": "rgb(255, 218, 185)", "color": "#fff" }
+    },
+    
+    {
+        date: 'March 2022 – May 2023',
+        title: 'Software Engineer',
+        subtitle: 'Mistral Solutions Private Limited, Bangalore, India',
         details: [
-            'Object-Oriented Programming',
-            'Embedded Systems',
-            'Introduction to C Programming',
-            'Introduction to Python Programming'
+            'Developed and deployed software solutions for embedded systems and robotics, integrating ROS2 Foxy with Python and C++ to support real-time communication between sensors and machine learning models.',
+            'Enhanced and optimized perception algorithms for image classification and object detection using TensorFlow and OpenCV, resulting in a 50% improvement in detection accuracy and a 25% gain in perception precision, enabling more reliable autonomy.',
+            'Collaborated with cross-functional hardware and firmware teams to optimize system architecture for autonomous robotics platforms, reducing data processing time by 60% and significantly boosting overall system response and efficiency in real-time environments.',
+            'Created internal tooling and dashboards to visualize high-frequency sensor data and machine learning model outputs. These tools were crucial in identifying system bottlenecks, streamlining debugging processes, and enhancing performance diagnostics.', 
+            'Contributed to a full product lifecycle—from prototyping to deployment—for autonomous robotics and smart automation.'
         ],
         iconStyle: { background: 'rgb(255, 182, 193)', color: '#fff' },
     },
+    {
+        date: 'March 2019 - Feb 2022',
+        title: 'Web Development Consultant',
+        subtitle: 'The Grafician, Chennai, India',
+        details: [
+            'Redesigned and developed responsive, user-friendly websites for a diverse set of clients, focusing on clean UI/UX and performance optimization using React.js, HTML5, CSS3, and JavaScript.',
+            'Translated Figma and Adobe XD designs into pixel-perfect pages aligned with brand guidelines.',
+            'Optimized performance with lazy loading, code splitting, and minification. Integrated RESTful APIs and handled client-side routing using React Router for dynamic user interactions and smooth navigation.',
+            'Ensured cross-browser compatibility and responsive layouts through extensive testing and use of frameworks like TailwindCSS.'
+        ],
+        iconStyle: { background: 'rgb(202,183,220)', color: '#fff' },
+    },
+    
     {
         date: 'August 2023 - May 2025',
         title: 'Master of Science in Computer Science',
@@ -31,42 +57,21 @@ export const EDUCATION_TIMELINE = [
         ],
         iconStyle: { background: 'rgb(255, 218, 185)', color: '#fff' },
     },
+    
+
     {
-        date: 'July 2022 - November 2022',
-        title: 'Web Developer Intern',
-        subtitle: 'The Grafician, Chennai, India',
+        date: 'May 2018 - May 2022',
+        title: 'Bachelor of Engineering in Electrical and Electronics',
+        subtitle: 'Anna University, India',
         details: [
-            'Played a pivotal role in enhancing website functionality through collaborative brainstorming.',
-            'Mastered Python and JavaScript, tackling complex challenges and delivering precise solutions.',
-            'Fostered seamless collaboration between development and design teams.',
-            'Carried out A/B testing methodologies to refine website features, improving conversion rates by 10%.'
-        ],
-        iconStyle: { background: 'rgb(202,183,220)', color: '#fff' },
-    },
-    {
-        date: 'March 2022 – June 2022',
-        title: 'Software Design Intern',
-        subtitle: 'Mistral Solutions Private Limited, Bangalore, India',
-        details: [
-            'Orchestrated pioneering robotics initiatives with ROS 2 Foxy and Python.',
-            'Applied advanced machine learning algorithms to enhance autonomous systems\' intelligence.',
-            'Demonstrated mastery in front-end and back-end development.',
-            'Led collaborative efforts, driving innovation through effective communication and leadership.'
+            'Object-Oriented Programming',
+            'Embedded Systems',
+            'Introduction to C Programming',
+            'Introduction to Python Programming'
         ],
         iconStyle: { background: 'rgb(255, 182, 193)', color: '#fff' },
     },
-
-    {
-        "date": "September 2024 - Present",
-        "title": "Software Development Intern",
-        "subtitle": "Entertainment Technologists Development Corp., California, USA",
-        "details": [
-            "Engineered AI/ML models using TensorFlow, Keras, Pandas, and OpenCV to optimize media processes including image processing, video rendering, and content analysis",
-            "Increased model efficiency by 15% through fine-tuning algorithms for processing media inputs",
-            "Integrated front-end interfaces with AI/ML models, improving input data workflows and reducing processing time by 20%"
-        ],
-        "iconStyle": { "background": "rgb(255, 218, 185)", "color": "#fff" }
-    }
+    
     
 ];
 
